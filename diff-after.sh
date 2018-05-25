6d11f5a977d7e7783c40b3fc0929e9eedce7af7a
Wes Higbee <wes.mcclure@gmail.com>
2018-05-25 12:17:30 -0400

    diff titles

--- diff-after.sh
+++ diff-after.sh
@@ -2,19 +2,25 @@
 
 # todo tree dump from image snapshots during repro
 
+logmd() {
+    echo "$@" | highlight --syntax md -O ansi
+}
+
 show_diff(){
     # 02 = no-standout
+    logmd "\n## $1 vs $2\n"
     diff --old-group-format=$'\e[0;31m%<\e[0m' \
     --new-group-format=$'\e[0;32m%>\e[0m' \
     --unchanged-group-format=$'\e[0;02m%=\e[0m' \
-    $1 $2 
+    temp/${1}.txt temp/${2}.txt
+    
 }
 
 unset prev_s
 # iterate tags which conveniently are already extracted to *.txt files
 for s in $(docker image ls --format="{{.Tag}}" reproduce-2094 | sort -h); do
     if [[ -v prev_s ]]; then
-        show_diff temp/${prev_s}.txt temp/${s}.txt
+        show_diff $prev_s $s
     fi
     prev_s=$s
 done
