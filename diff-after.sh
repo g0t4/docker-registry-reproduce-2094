9dff0a18d29787b6b6c797981c6af8cd3bded254
Wes Higbee <wes.mcclure@gmail.com>
2018-05-25 12:03:42 -0400

    fix diff after

--- diff-after.sh
+++ diff-after.sh
@@ -14,7 +14,7 @@ unset prev_s
 # iterate tags which conveniently are already extracted to *.txt files
 for s in $(docker image ls --format="{{.Tag}}" reproduce-2094 | sort -h); do
     if [[ -v prev_s ]]; then
-        show_diff $prev_s $s
+        show_diff temp/${prev_s}.txt temp/${s}.txt
     fi
     prev_s=$s
 done
